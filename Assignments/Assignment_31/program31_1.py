f31c8a02a274587a29580d15dbe7eb8b22accaa8
Sakshib20 <154331302+Sakshib20@users.noreply.github.com>
2026-04-03 14:03:30 +0530

    Add Assignment31 solution Q1,Q2

---
+++ Assignments/Assignment_31/program31_1.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import schedule
+import time
+
+# //////////////////////////////////////////////////////////////////////////////////////
+# //
+# //  Function Name : DirScan
+# //  Description   : It accepts a file name  from user and 
+#                     returns the number of line in the file
+# //  Input         : String
+# //  Ouput         : Integer
+# //  Author        : Sakshi Pradeep Bhapkar
+# //  Date          : 25/3/2026
+# //
+# //////////////////////////////////////////////////////////////////////////////////////
+
+def DirScan(dName,extName):
+    Ret = os.path.exists(dName)
+
+    if(Ret == False):
+        print("There is no such directory")
+        return
+    
+    Ret = os.path.isdir(dName)
+
+    if(Ret == False):
+        print("Unable to scan as it's not a directory")
+        return
+    
+    else:
+
+        fobj = open("Marvellous.log",'w')
+
+        for FolderName, SubFolder, FileName in os.walk(dName):
+            for fName in FileName:
+                if fName.endswith(extName):
+                    fobj.write(fName)
+                    fobj.write("\n")
+
+
+def main():
+    dName = sys.argv[1]
+    extName = sys.argv[2]
+
+    if(len(sys.argv) != 3):
+        print("Please enter correct arguments")
+        print("1 : Folder name")
+        print("2 : Extension")
+        return
+    
+    else:
+        
+        schedule.every(30).seconds.do(DirScan, sys.argv[1],sys.argv[2])
+
+        while(True):
+            schedule.run_pending()
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Assignments/Assignment_31/program31_2.py
@@ -0,0 +1,65 @@
+import os
+import sys
+import schedule
+import time
+from pathlib import Path
+
+# //////////////////////////////////////////////////////////////////////////////////////
+# //
+# //  Function Name : CountWords
+# //  Description   : It accepts a file name  from user and 
+#                     returns the number of line in the file
+# //  Input         : String
+# //  Ouput         : Integer
+# //  Author        : Sakshi Pradeep Bhapkar
+# //  Date          : 25/3/2026
+# //
+# //////////////////////////////////////////////////////////////////////////////////////
+
+
+def DirRename(dName,src,dest):
+    Ret = os.path.exists(dName)
+
+    if(Ret == False):
+        print("There is no such directory")
+        return
+    
+    Ret = os.path.isdir(dName)
+
+    if(Ret == False):
+        print("Unable to scan as it's not a directory")
+        return
+    
+    else:
+
+        for FolderName, SubFolder, FileName in os.walk(dName):
+            for fName in FileName:
+                if fName.endswith(src):
+                    fPath = Path(FolderName)/fName
+                    fPath.rename(fPath.with_suffix(dest))
+
+
+def main():
+    dName = sys.argv[1]
+    ext1 = sys.argv[2]
+    ext2 = sys.argv[3]
+
+    if(len(sys.argv) != 4):
+        print("Please enter correct arguments")
+        print("1 : Folder name")
+        print("2 : Original Extension")
+        print("3 : Replacement Extension : ")
+        return
+    
+    
+    else:
+        
+        schedule.every(30).seconds.do(DirRename,sys.argv[1],sys.argv[2],sys.argv[3])
+
+        while(True):
+            schedule.run_pending()
+            time.sleep(1)
+        
+
+if __name__ == "__main__":
+    main()
